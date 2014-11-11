83ebbfe282f20863e17eca632835060c36b2d419
Yevhen Osadchyi <silver_od@Yevhens-MacBook-Air.local>
2014-11-11 23:46:28 +0200

    Refactoring

--- forking.pl
+++ forking.pl
@@ -8,68 +8,68 @@ use FCGI;
 my $term = 0;
 
 $\ = "\n";
-$SIG{TERM} = sub{};
 $SIG{INT} = sub { $term = 1 };
 
-if(scalar @ARGV < 3) {
-    print 'Error. Usage: $path $backlog $count';
-    exit();
-}
+&main;
 
-my $path    = $ARGV[0];
-my $backlog = int($ARGV[1]);
-my $count   = int($ARGV[2]);
-
-print "[$$] Starting manager...";
-
-my $socket = FCGI::OpenSocket($path, $backlog);
-if(!$socket) {
-    print "Error. Failed to open socket.";
-    exit();
-}
-
-for (1..$count) {
-    my $pid = fork();
-
-    if($pid) {
-        print "[$$] Started FCGI script $pid";
-        print "[$$] Working...\n";
-
-        FCGI::CloseSocket($socket);
-
-        print "[$$] Closed $path SOCKET in manager";
-
-    } else {
-        print "[$$] I am FCGI script";
-
-        my $count = 0;
-        my $request = FCGI::Request(
-           \*STDIN, \*STDOUT, \*STDERR,
-           \%ENV, int($socket || 0), 1
-        );
-
-        while($request->Accept() >= 0) {
-            print "Content-type:text/plain;charset=utf-8\r\n\r\n";
-            print "[$$]";
-            print "Counter: $count";
-            ++$count;
-        }
-
-        print "[$$] FCGI exiting...";
-        exit(0);
+sub main {
+    if(scalar @ARGV < 3) {
+        print 'Error. Usage: $path $backlog $count';
+        exit();
     }
+
+    my $path    = $ARGV[0];
+    my $backlog = int($ARGV[1]);
+    my $count   = int($ARGV[2]);
+
+    print "[$$-Manager] Starting...";
+
+    my $socket = FCGI::OpenSocket($path, $backlog);
+    if(!$socket) {
+        print "Error. Failed to open socket.";
+        exit();
+    }
+
+    for (1..$count) {
+        my $pid = fork();
+        process_fcgi($socket) unless($pid);
+    }
+
+    print "[$$-Manager] Working...\n";
+
+    print "[$$-Manager] Closing socket $path ...";
+    FCGI::CloseSocket($socket);
+
+    while(!$term) {
+        sleep(2);
+    }
+
+    print "[$$-Manager] Terminating all FCGIs...";
+        #kill('TERM', $pid);
+    sleep(3);
+        #print "[$$] Closed $pid";
+    print "[$$-Manager] Exiting...\n";
+    exit(0);
 }
 
-while(!$term) {
-    sleep(2);
+sub process_fcgi {
+    my $socket = shift;
+
+    print "[$$-FCGI] Created";
+
+    my $count = 0;
+    my $request = FCGI::Request(
+       \*STDIN, \*STDOUT, \*STDERR,
+       \%ENV, int($socket || 0), 1
+    );
+
+    while($request->Accept() >= 0) {
+        print "Content-type:text/plain;charset=utf-8\r\n\r\n";
+        print "[$$]";
+        print "Counter: $count";
+        ++$count;
+    }
+
+    print "[$$-FCGI] Closing...";
+    exit(0);
 }
-
-print "[$$] Closing all FCGI childs...";
-    #kill('TERM', $pid);
-
-sleep(3);
-    #print "[$$] Closed $pid";
-
-
-print "[$$] Exiting manager...\n";
-exit(0);
