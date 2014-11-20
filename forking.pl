16f6d72bf18658853d1f98e6e9388b69795fa1c7
Yevhen Osadchyi <silver_od@Yevhens-MacBook-Air.local>
2014-11-20 16:56:28 +0200

    Created FCGI instances. Creates DB process with IPC. BUG: DB is not closing on terminate

--- forking.pl
+++ forking.pl
@@ -4,17 +4,20 @@ use strict;
 use warnings;
 
 use FCGI;
+use Data::Dumper;
 
-my $term = 0;
+my $terminate = 0;
 
 $\ = "\n";
-$SIG{INT} = sub { $term = 1 };
+
+$SIG{INT} = sub { $terminate = 1 };
+$SIG{CHLD} = sub {};
 
 &main;
 
 sub main {
     if(scalar @ARGV < 3) {
-        print 'Error. Usage: $path $backlog $count';
+        print "[$$-Manager] Error. Usage: path backlog count";
         exit();
     }
 
@@ -26,34 +29,50 @@ sub main {
 
     my $socket = FCGI::OpenSocket($path, $backlog);
     if(!$socket) {
-        print "Error. Failed to open socket.";
+        print "[$$-Manager] Error. Failed to open socket.";
         exit();
     }
 
-    for (1..$count) {
-        my $pid = fork();
-        process_fcgi($socket) unless($pid);
+pipe(FCGI_READ, FCGI_WRITE);
+pipe(DB_READ, DB_WRITE);
+
+FCGI_WRITE->autoflush(1);
+DB_WRITE->autoflush(1);
+
+    my $pid;
+    my %pids;
+
+    unless ($pid = fork()) {
+        close(FCGI_READ) or warn "[$$-DB] Can't close FCGI_READ";
+        process_db(\*DB_READ, \*FCGI_WRITE);
+        exit(0);
     }
 
-    print "[$$-Manager] Working...\n";
+    $pids{$pid} = 'DB';
 
-    print "[$$-Manager] Closing socket $path ...";
+    for (1..$count) {
+        unless($pid = fork()) {
+            close(DB_READ) or warn "[$$-FCGI] Can't close DB_READ";
+            process_fcgi(\*FCGI_READ, \*DB_WRITE, $socket);
+            exit(0);
+        }
+        $pids{$pid} = 'FCGI';
+    }
+
+    print "[$$-Manager] Stated all childs. Waiting for signal...\n";
+
     FCGI::CloseSocket($socket);
 
-    while(!$term) {
-        sleep(2);
-    }
+    print "[$$-Manager] Closed socket";
+    sleep while(!$terminate);
 
-    print "[$$-Manager] Terminating all FCGIs...";
-        #kill('TERM', $pid);
-    sleep(3);
-        #print "[$$] Closed $pid";
-    print "[$$-Manager] Exiting...\n";
+    print "[$$-Manager] Terminating all FCGIs and exiting...\n";
+
     exit(0);
 }
 
 sub process_fcgi {
-    my $socket = shift;
+    my ($FCGI_READ, $DB_WRITE, $socket) = @_;
 
     print "[$$-FCGI] Created";
 
@@ -68,8 +87,31 @@ sub process_fcgi {
         print "[$$]";
         print "Counter: $count";
         ++$count;
+
+        print $DB_WRITE "$count";
     }
 
     print "[$$-FCGI] Closing...";
-    exit(0);
+
+    close($DB_WRITE)    or warn "[$$-FCGI] Can\'t close \$DB_WRITE. Reason: $@";
+    close($FCGI_READ)   or warn "[$$-FCGI] Can\'t close \$FCGI_READ. Reason: $@";
+}
+
+sub process_db {
+    my ($DB_READ, $FCGI_WRITE) = @_;
+    my $data;
+
+    print "[$$-DB] Started";
+
+    my $response;
+    while($response = <$DB_READ>) {
+        chomp($response);
+        #last if($response eq 'close');
+        print STDOUT "[$$-DB] Response from FCGI: \"$response\"";
+    }
+
+    print "[$$-DB] Closing...";
+
+    close($DB_READ)     or warn "[$$-DB] Can\'t close \$DB_READ";
+    close($FCGI_WRITE)  or warn "[$$-DB] Can\'t close \$FCGI_WRITE";
 }
